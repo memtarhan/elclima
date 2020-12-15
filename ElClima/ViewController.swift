ad3b6f30238f95ebad03bd7610531ba21681fce0
Mehmet Tarhan <memtarhan@gmail.com>
2020-12-15 23:24:57 +0300

    Created project

---
+++ ElClima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ElClima
+//
+//  Created by Mehmet Tarhan on 15.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
